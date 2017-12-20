45a05f114c4d561cfee1f7cb45137abbc0fd19f3
DrBloke <sam@samtracey.co.uk>
2017-12-20 09:32:14 +0000

    Delete reducer_books.js

--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function () {
-  return [
-    {title: 'Harry Potter'},
-    {title: 'Gospel according to Satan'},
-    {title: 'How to win friends and influence people'},
-    {title: 'On flew over the cuckoos nest'}
-  ]
-}
